9af4847f8b699995090693f19af2d9dae6c1c2eb
Ronak J Vanpariya <26689210+vanpariyar@users.noreply.github.com>
2022-06-24 14:07:16 +0530

    js changes for null

--- script.js
+++ script.js
@@ -9,18 +9,18 @@ var url = new URL(url_string);
 var type = url.searchParams.get("type");
 var value = url.searchParams.get("value");
 
-const studentname = decodeURIComponent(url.searchParams.get('name'));
-const purpose = decodeURIComponent(url.searchParams.get("purpose"));
-const departmentType = decodeURIComponent(url.searchParams.get("department"))
-const startDate = decodeURIComponent(url.searchParams.get("start"));
-const endDate = decodeURIComponent(url.searchParams.get("end"));
-const certificateId = decodeURIComponent(url.searchParams.get("certificate_id"));
+const studentname = decodeURIComponent(url.searchParams.get('name')) != "null" ? decodeURIComponent(url.searchParams.get('name')) : ''
+const purpose = decodeURIComponent(url.searchParams.get("purpose")) != "null" ? decodeURIComponent(url.searchParams.get("purpose")) : ''
+const departmentType = decodeURIComponent(url.searchParams.get("department")) != "null" ? decodeURIComponent(url.searchParams.get("department")) : ''
+const startDate = decodeURIComponent(url.searchParams.get("start")) != "null" ? decodeURIComponent(url.searchParams.get("start")) : ''
+const endDate = decodeURIComponent(url.searchParams.get("end")) != "null" ? decodeURIComponent(url.searchParams.get("end")) : ''
+const certificateId = decodeURIComponent(url.searchParams.get("certificate_id")) != "null" ? decodeURIComponent(url.searchParams.get("certificate_id")) : ''
 
 const validationText = `This certificate is provided to <strong>${studentname}</strong> 👏 for ${purpose} 🏆 in ${departmentType} for date between ${startDate} and ${endDate} and 🙂 Unique Certificate ID is <strong>${certificateId}</strong> `;
 
 text.innerHTML = validationText;
 
-if( studentname != '' && purpose != '' && departmentType != '' && startDate != '' && endDate != '' && certificateId != '' ) {
+if( (studentname != '') && (purpose != '') && (departmentType != '') && (startDate != '') && (endDate != '') && (certificateId != '') ) {
     certificateContainer.classList.remove('d-none');
 } else {  
     invalidCertificateContainer.classList.remove('d-none');
